59d5c4ff93f0148ec0cde721cc7c6736c40df888
Prongt <eahern123@gmail.com>
2019-09-22 22:59:44 +0100

    Unit test stuff

---
+++ S.D.A.T/Assets/Scripts/DamageCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCalculator 
+{
+    public static int CalculateDamage(int amount, float mitigationPercent)
+    {
+        float multiplier = 1f - mitigationPercent;
+        return Convert.ToInt32(amount * multiplier);
+    }
+}
---
+++ S.D.A.T/Assets/Tests/damage_calculator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class damage_calculator
+    {
+
+        [Test]
+        public void CalculateDamage_setsDamageToHalfWith50PercentMitigation()
+        {
+            int finalDamage = DamageCalculator.CalculateDamage(10, 0.5f);
+            Assert.AreEqual(5, finalDamage);
+        }
+        
+        [Test]
+        public void CalculateDamage_setsDamageTo2With80PercentMitigation()
+        {
+            int finalDamage = DamageCalculator.CalculateDamage(10, .80f);
+            Assert.AreEqual(2, finalDamage);
+        }
+
+    }
+}
