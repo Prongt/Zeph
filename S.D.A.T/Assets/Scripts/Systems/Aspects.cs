2719419b66c637708c2e251e29b063bc670c4979
Prongt <eahern123@gmail.com>
2019-09-26 22:47:44 +0100

    Working on elemental properties

---
+++ S.D.A.T/Assets/Scripts/Systems/Aspects.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aspects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
+
+[Serializable]
+public enum AspectTypes
+{
+    Burnable,
+    Wetable,
+    Growable,
+    Damagable,
+    Throwable
+    
+}
---
+++ S.D.A.T/Assets/Scripts/Systems/Interactable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Interactable : MonoBehaviour
+{
+    [Header("ElementalProperties")]
+    [SerializeField] private ElementalMaterial elementalMaterial;
+    [SerializeField] private MaterialProperties[] additionalProperties;
+
+    private List<MaterialProperties> exposedProperties;
+
+    private void Awake()
+    {
+        exposedProperties = new List<MaterialProperties>();
+        InitalizeProperties();
+    }
+
+    private void InitalizeProperties()
+    {
+        foreach (MaterialProperties props in elementalMaterial.MaterialProperties)
+        {
+            AddProperties(props);
+        }
+
+        foreach (MaterialProperties props in additionalProperties)
+        {
+            AddProperties(props);
+        }
+    }
+
+    private void OnValidate()
+    {
+        InitalizeProperties();
+    }
+
+    void AddProperties(MaterialProperties propertyToAdd)
+    {
+        if (!exposedProperties.Contains(propertyToAdd))
+        {
+            exposedProperties.Add(propertyToAdd);
+        }
+    }
+}
