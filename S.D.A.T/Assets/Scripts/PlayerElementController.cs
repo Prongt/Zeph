56f71f42a0d169df4689b06b34baaeb98391f87f
Eoin Ahern <eahern123@gmail.com>
2019-10-04 17:12:42 +0100

    Working on Element system

--- S.D.A.T/Assets/Scripts/PlayerElementController.cs
+++ S.D.A.T/Assets/Scripts/PlayerElementController.cs
@@ -5,24 +5,86 @@ using UnityEngine;
 
 public class PlayerElementController : MonoBehaviour
 {
-    public float[] elementRadius;
+    public PlayerElement[] elementData;
+    [SerializeField] private bool drawGizmos = false;
+    [SerializeField] private float gizmoHeight;
+    
+    [SerializeField] private KeyCode powerKey;
+    
+    //number of collisions detected for each element
+    private const int maxAffectableObjects = 25;
+
+
+    private void Awake()
+    {
+        for (int i = 0; i < elementData.Length; i++)
+        {
+            elementData[i].colliders = new Collider[25];
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(powerKey))
+        {
+            for (int i = 0; i < elementData.Length; i++)
+            {
+                var size = Physics.OverlapSphereNonAlloc(transform.position, elementData[i].Range, elementData[i].colliders);
+                for (int j = 0; j < elementData[i].colliders.Length; j++)
+                {
+                    var objec = elementData[i].colliders[j];
+                    if (objec)
+                    {
+                        var obj = objec.GetComponent<Interactable>();
+                        if (obj)
+                        {
+                            obj.ApplyElement(elementData[i].Element);
+                        }   
+                    }
+                }
+            }
+        }
+    }
+
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.white;
-        Transform t = GetComponent<Transform>();
-        float theta = 0;
-        float x = elementRadius[0] * Mathf.Cos(theta);
-        float y = elementRadius[0] * Mathf.Sin(theta);
-        Vector3 pos= t.position + new Vector3(x,0,y);
-        Vector3 newPos= pos;
-        Vector3 lastPos= pos;
-        for(float thetaLoop = 0.1f; thetaLoop < Mathf.PI*2; thetaLoop += 0.1f){
-            x = elementRadius[0] * Mathf.Cos(thetaLoop);
-            y = elementRadius[0] * Mathf.Sin(thetaLoop);
-            newPos = t.position + new Vector3(x,0,y);
-            Gizmos.DrawLine(pos,newPos);
-            pos = newPos;
+        if (drawGizmos == false)
+        {
+            return;
         }
-        Gizmos.DrawLine(pos,lastPos);
+
+        
+        for (int i = 0; i < elementData.Length; i++)
+        {
+            Gizmos.color = elementData[i].DebugColor;
+            Transform t = GetComponent<Transform>();
+            float theta = 0;
+            float x = elementData[i].Range * Mathf.Cos(theta);
+            float y = elementData[i].Range * Mathf.Sin(theta);
+            Vector3 pos= t.position + new Vector3(x,gizmoHeight,y);
+            Vector3 newPos= pos;
+            Vector3 lastPos= pos;
+            for(float thetaLoop = 0.1f; thetaLoop < Mathf.PI*2; thetaLoop += 0.1f){
+                x = elementData[i].Range * Mathf.Cos(thetaLoop);
+                y = elementData[i].Range * Mathf.Sin(thetaLoop);
+                newPos = t.position + new Vector3(x,0,y);
+                Gizmos.DrawLine(pos,newPos);
+                pos = newPos;
+            }
+            Gizmos.DrawLine(pos,lastPos);
+            //Gizmos.DrawWireSphere(transform.position, elementData[i].Range);
+        }
     }
+
 }
+
+[Serializable]
+public struct PlayerElement
+{
+    public Element Element;
+    public float Range;
+    public Color DebugColor;
+    [HideInInspector] public Collider[] colliders;
+}
+
+
--- S.D.A.T/Assets/Scripts/Systems/Element.cs
+++ S.D.A.T/Assets/Scripts/Systems/Element.cs
@@ -10,7 +10,7 @@ public class Element : ScriptableObject
     //[SerializeField] private ElementsEnum elementEnum;
     [SerializeField] private List<AspectType> promotes;
     [SerializeField] private List<AspectType> negates;
-
+    
     public List<AspectType> Promotes => promotes;
 
     public List<AspectType> Negates => negates;
