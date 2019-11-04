940e2fac9d702ced784b9252a851e172b8afb328
Eoin Ahern <eahern123@gmail.com>
2019-11-04 17:48:47 +0000

    Testing gravity movement

--- Zeph/Assets/Scripts/Movement/PlayerMove.cs
+++ Zeph/Assets/Scripts/Movement/PlayerMove.cs
@@ -14,6 +14,13 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private FloatReference playerTurnSpeed;
     [SerializeField] private FloatReference jumpForce;
 
+    public float speed = 90f;
+    public float turnSpeed = 5f;
+    public float hoverForce = 65f;
+    public float hoverHeight = 3.5f;
+    
+    public float sideMoveMultiplier;
+    public float upMoveMultiplier;
     private float distanceToGround;
     
     
@@ -31,8 +38,69 @@ public class PlayerMove : MonoBehaviour
     
     void FixedUpdate()
     {
+//        if (!GravityDistortion.useNewGravity)
+//        {
+//            Move();
+//            Jump();
+//        }
+//        else
+//        {
+//            AltMove();
+//            AltJump();
+//        }
+        //AltMove();
+        
         Move();
         Jump();
+    }
+
+    private void AltJump()
+    {
+        
+    }
+
+    private void AltMove()
+    {
+//        Vector3 newRight = Vector3.Cross(Physics.gravity, transform.forward);
+//        Vector3 newForward = Vector3.Cross(newRight, Physics.gravity);
+//        var moveSpeed = Time.deltaTime * playerSpeed.Value;
+//        
+//        //Movement
+//        Vector3 movement = new Vector3();
+//        
+//        movement += newRight* (moveSpeed * -Input.GetAxis("Horizontal")) * sideMoveMultiplier; 
+//        movement += newForward * (moveSpeed * Input.GetAxis("Vertical")) * upMoveMultiplier;
+//        myBody.MovePosition(myBody.position + (movement * playerSpeed.Value));
+//        
+//        
+//        
+//        Vector3 heading = Vector3.Normalize(movement);
+//        Vector3 lerpForward = math.lerp((float3) transform.forward, (float3) heading,
+//            Time.deltaTime * playerTurnSpeed.Value);
+//        transform.forward = new Vector3(lerpForward.x, 0, lerpForward.z);
+
+        Ray ray = new Ray (transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, hoverHeight))
+        {
+            float proportionalHeight = (hoverHeight - hit.distance) / hoverHeight;
+            Vector3 appliedHoverForce = Vector3.up * proportionalHeight * hoverForce;
+            myBody.AddForce(appliedHoverForce, ForceMode.Acceleration);
+        }
+        
+        Vector3 movement = new Vector3();
+        movement += right * (playerSpeed.Value * Input.GetAxis("Horizontal")); 
+        movement += forward * (playerSpeed.Value * Input.GetAxis("Vertical"));
+        var test = movement * playerSpeed.Value;
+        //myBody.MovePosition(myBody.position + (movement * playerSpeed.Value));
+        myBody.AddRelativeForce(test.x, 0, test.z);
+        
+                Vector3 heading = Vector3.Normalize(movement);
+        Vector3 lerpForward = math.lerp((float3) transform.forward, (float3) heading,
+            Time.deltaTime * playerTurnSpeed.Value);
+        transform.forward = new Vector3(lerpForward.x, 0, lerpForward.z);
+        //myBody.AddRelativeTorque(0f, Input.GetAxis ("Horizontal") * turnSpeed, 0f);
     }
 
     private void Jump()
---
+++ Zeph/Assets/Scripts/Systems/GravityDistortion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityDistortion : MonoBehaviour
+{
+    public Vector3 newGravity;
+    private Vector3 ogGravity;
+
+    public static bool useNewGravity = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ogGravity = Physics.gravity;
+    }
+
+    // Update is called once per frame
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.Q))
+//        {
+//            Physics.gravity = Vector3.up;
+//        }
+//        if (Input.GetKeyDown(KeyCode.E))
+//        {
+//            Physics.gravity = Vector3.down;
+//        }
+//    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Physics.gravity = newGravity;
+            useNewGravity = true;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Physics.gravity = ogGravity;
+            useNewGravity = false;
+        }
+    }
+}
