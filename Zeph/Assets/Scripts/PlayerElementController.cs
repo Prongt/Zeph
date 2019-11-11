154b8c5be3afc19edd4ecf9f3ee13fca06b47f8f
Eoin Ahern <eahern123@gmail.com>
2019-11-11 12:54:12 +0000

    Misc

--- Zeph/Assets/Scripts/PlayerElementController.cs
+++ Zeph/Assets/Scripts/PlayerElementController.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class PlayerElementController : MonoBehaviour
 {
     public PlayerElement[] elementData;
+    [SerializeField] private float height;
     [SerializeField] private bool drawGizmos = false;
     [HideIf("drawGizmos", true)][SerializeField] private float gizmoHeight;
 
@@ -58,6 +59,8 @@ public class PlayerElementController : MonoBehaviour
             {
                 StartCoroutine(LightFade());
             }
+            
+            //Vector2 range
 
             for (int i = 0; i < elementData.Length; i++)
             {
@@ -66,6 +69,7 @@ public class PlayerElementController : MonoBehaviour
                 for (int j = 0; j < elementData[i].colliders.Length; j++)
                 {
                     var objec = elementData[i].colliders[j];
+                    //if (objec.transform.position.y )
                     if (objec)
                     {
                         var obj = objec.GetComponent<Interactable>();
@@ -111,29 +115,40 @@ public class PlayerElementController : MonoBehaviour
             return;
         }
 
+        Vector3 topGizmo = transform.position;
+        topGizmo.y += height;
 
+        Vector3 bottomGizmo = transform.position;
+        bottomGizmo.y -= height;
+
+        DrawGizmosAtHeight(topGizmo);
+        DrawGizmosAtHeight(bottomGizmo);
+    }
+
+    private void DrawGizmosAtHeight(Vector3 position)
+    {
         for (int i = 0; i < elementData.Length; i++)
         {
             Gizmos.color = elementData[i].Element.DebugColor;
-            Transform t = GetComponent<Transform>();
             float theta = 0;
             float x = elementData[i].Element.PlayerRange * Mathf.Cos(theta);
             float y = elementData[i].Element.PlayerRange * Mathf.Sin(theta);
-            Vector3 pos= t.position + new Vector3(x,gizmoHeight,y);
-            Vector3 newPos= pos;
-            Vector3 lastPos= pos;
-            for(float thetaLoop = 0.1f; thetaLoop < Mathf.PI*2; thetaLoop += 0.1f){
+            Vector3 pos = position + new Vector3(x, gizmoHeight, y);
+            Vector3 newPos = pos;
+            Vector3 lastPos = pos;
+            for (float thetaLoop = 0.1f; thetaLoop < Mathf.PI * 2; thetaLoop += 0.1f)
+            {
                 x = elementData[i].Element.PlayerRange * Mathf.Cos(thetaLoop);
                 y = elementData[i].Element.PlayerRange * Mathf.Sin(thetaLoop);
-                newPos = t.position + new Vector3(x,0,y);
-                Gizmos.DrawLine(pos,newPos);
+                newPos = position + new Vector3(x, 0, y);
+                Gizmos.DrawLine(pos, newPos);
                 pos = newPos;
             }
-            Gizmos.DrawLine(pos,lastPos);
+
+            Gizmos.DrawLine(pos, lastPos);
             //Gizmos.DrawWireSphere(transform.position, elementData[i].Range);
         }
     }
-
 }
 
 [Serializable]
