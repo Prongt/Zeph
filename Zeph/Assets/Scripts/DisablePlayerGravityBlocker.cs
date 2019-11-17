fe8ccd6de30ff558f6b86d670d007761c69ff6ae
Prongt <1413840+Prongt@users.noreply.github.com>
2019-11-17 22:08:40 +0000

    Tree firespread adjusted and added gravity zone

--- Zeph/Assets/Scripts/DisablePlayerGravityBlocker.cs
+++ Zeph/Assets/Scripts/DisablePlayerGravityBlocker.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class DisablePlayerGravityBlocker : MonoBehaviour
 {
+    public static int count = 0;
+    private int myCount;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            count++;
+            myCount++;
             PlayerMove.PlayerUsesGravity = false;
         }
     }
@@ -16,6 +20,25 @@ public class DisablePlayerGravityBlocker : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
+            count--;
+            myCount--;
+            if (count <= 0)
+            {
+                PlayerMove.PlayerUsesGravity = true;
+            }
+           
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (myCount > 0)
+        {
+            myCount--;
+            count--;
+        }
+        if (PlayerMove.PlayerUsesGravity == false)
         {
             PlayerMove.PlayerUsesGravity = true;
         }
