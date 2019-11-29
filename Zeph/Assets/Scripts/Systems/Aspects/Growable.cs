8311622adcc1787db1c821ee7f67e97b8e955cf1
Ryan-Higgins <c16381886@mydit.ie>
2019-11-29 13:04:27 +0000

    Working on implenting the ivy bridge + Distortion ground.

--- Zeph/Assets/Scripts/Systems/Aspects/Growable.cs
+++ Zeph/Assets/Scripts/Systems/Aspects/Growable.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Growable : Aspects
@@ -6,6 +8,9 @@ public class Growable : Aspects
     [SerializeField] private Material GrowingMaterial;
     [SerializeField] private GameObject GrowingParticleEffect;
     public Animator myAnim;
+    [SerializeField] private Material mat;
+    private float matX;
+    [SerializeField] private Animator groundDistort;
 
     
     public Type[] componentTypes = new Type[]
@@ -23,14 +28,42 @@ public class Growable : Aspects
    protected override void Initialize()
     {
         base.Initialize();
-        myAnim = GetComponent<Animator>();
+        if (gameObject.CompareTag("Plant"))
+        {
+            myAnim = GetComponent<Animator>();
+        }
+
+        if (gameObject.CompareTag("Bridge"))
+        {
+            mat = gameObject.GetComponent<MeshRenderer>().material;
+            matX = mat.GetFloat("Vector1_D0BABF75");
+        }
     }
+
+   void Update()
+   {
+       if (gameObject.CompareTag("Bridge"))
+       {
+           mat.SetFloat("Vector1_D0BABF75", matX);
+       }
+   }
     
     public override void Promote(Transform source = null, Element element = null)
     {
         base.Promote(source, element);
-        myAnim.SetBool("Growing", true);
+        print("IM GROWING");
+        if (gameObject.CompareTag("Plant"))
+        {
+            myAnim.SetBool("Growing", true);
+        }
         //gameObject.GetComponent<Renderer>().material = GrowingMaterial;
+        if (gameObject.CompareTag("Bridge"))
+        {
+            if (matX >= 1)
+            {
+                StartCoroutine(Appear());
+            }
+        }
     }
 
     public override void Negate(Transform source = null)
@@ -38,5 +71,39 @@ public class Growable : Aspects
         //Stop growing
         //Debug.Log("Shrinking");
         base.Promote(source);
+    }
+
+    IEnumerator Appear()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (!groundDistort.GetBool("Animate"))
+        {
+            if (matX >= 1)
+            {
+                matX -= 15 * Time.deltaTime;
+                StartCoroutine(Appear());
+            }
+            else
+            {
+                gameObject.GetComponent<BoxCollider>().center = new Vector3(-6.721177f,-3.552706e-16f, 0.1f);
+                gameObject.GetComponent<BoxCollider>().size = new Vector3(18.35765f,3,0.2f);
+                gameObject.GetComponent<BoxCollider>().isTrigger = false;
+                StopCoroutine(Appear());
+            }
+        } else if (groundDistort.GetBool("Animate"))
+        {
+            if (matX >= -13)
+            {
+                matX -= 15 * Time.deltaTime;
+                StartCoroutine(Appear());
+            }
+            else
+            {
+                gameObject.GetComponent<BoxCollider>().center = new Vector3(0,2.384186e-08f, 0.1f);
+                gameObject.GetComponent<BoxCollider>().size = new Vector3(31.76197f,3,0.2f);
+                gameObject.GetComponent<BoxCollider>().isTrigger = false;
+                StopCoroutine(Appear());
+            }
+        }
     }
 }
