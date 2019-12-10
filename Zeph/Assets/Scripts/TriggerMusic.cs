0dde890cce03f92c9c5f881202f5d19674c8f63c
Eoin Ahern <eahern123@gmail.com>
2019-12-10 13:54:05 +0000

    Music loops between scenes

--- Zeph/Assets/Scripts/TriggerMusic.cs
+++ Zeph/Assets/Scripts/TriggerMusic.cs
@@ -3,16 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using FMODUnity;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerMusic : MonoBehaviour
 {
     [SerializeField] private StudioEventEmitter studioEventEmitter;
-
+    [SerializeField] private bool DontDestroyOnLoad = false;
     private void Start()
     {
         GrabComponents();
         studioEventEmitter.Play();
+        if (DontDestroyOnLoad)
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
+
+    
 
     private void OnValidate()
     {
