fbc087e06a07538aeb14463b17bf930377051812
Ryan-Higgins <c16381886@mydit.ie>
2020-02-09 18:03:15 +0000

    Cave puzzle pretty much complete

---
+++ Zeph/Assets/CrystalAttach.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalAttach : MonoBehaviour
+{
+    [SerializeField] private Transform crystalPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider crystal)
+    {
+        if (crystal.CompareTag("Crystal"))
+        {
+            crystal.gameObject.transform.position = crystalPos.position;
+            crystal.gameObject.transform.rotation = crystalPos.rotation;
+            crystal.gameObject.transform.localScale = crystalPos.localScale;
+            crystal.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            crystal.GetComponent<Chargeable>().attached = true;
+        }   
+    }
+}
