c60ac99a6db2c38feeac46d5e6719aef27de975b
Ryan-Higgins <c16381886@mydit.ie>
2020-02-21 17:52:42 +0000

    Basic Checkpoint added

---
+++ Zeph/Assets/CheckpointManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public List<GameObject> checkpoints;
+    public static GameObject curCheckpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curCheckpoint = checkpoints[0];
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("hit Trigger");
+            curCheckpoint = checkpoints[1];
+        }
+    }
+}
---
+++ Zeph/Assets/FallReset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallReset : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerMove>().enabled = false;
+            Physics.gravity = new Vector3(0,-9.81f,0);
+            other.transform.position = CheckpointManager.curCheckpoint.transform.position;
+            other.transform.rotation = CheckpointManager.curCheckpoint.transform.rotation;
+            StartCoroutine(Delay());
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(1);
+        GameObject.Find("Zeph").GetComponent<PlayerMove>().enabled = true;
+    }
+}
