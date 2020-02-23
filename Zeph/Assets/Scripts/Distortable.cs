0a0beae5bbc3e46f2b2925be534e1f418aa290b8
Eoin Ahern <eahern123@gmail.com>
2020-02-23 10:36:22 +0000

    Fixed conductor and colliders on distortion bridge

---
+++ Zeph/Assets/Scripts/Distortable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class Distortable : MonoBehaviour
+{
+    [SerializeField] private Animator myAnim;
+    [SerializeField] private Colliders colliders;
+    private static readonly int distort = Animator.StringToHash("Distort");
+    
+
+    private void Start()
+    {
+        myAnim = GetComponentInChildren<Animator>();
+        
+        if (colliders.normal && colliders.distort)
+        {
+            colliders.normal.enabled = true;
+            colliders.distort.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Distortion.IsDistorting)
+        {
+            if (colliders.normal && colliders.distort)
+            {
+                colliders.distort.enabled = true;
+                colliders.normal.enabled = false;
+            }
+        }
+        else
+        {
+            if (colliders.normal && colliders.distort)
+            {
+                colliders.normal.enabled = true;
+                colliders.distort.enabled = false;
+            }
+        }
+        
+        myAnim.SetBool(distort, Distortion.IsDistorting);
+    }
+
+    [Serializable]
+    private struct Colliders
+    {
+        public Collider normal;
+        public Collider distort;
+    }
+}
