432d93e575eb253e08ae96443d29ccb87b7b046f
Eoin Ahern <eahern123@gmail.com>
2020-02-24 11:59:42 +0000

    Added box respawn component

---
+++ Zeph/Assets/Scripts/RespawnWhenOutOfBounds.cs
@@ -0,0 +1,24 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class RespawnWhenOutOfBounds : MonoBehaviour
+{
+    [SerializeField][TagField] private new string tag;
+    private Vector3 startPos;
+    private Quaternion startRot;
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        startRot = transform.rotation;
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(tag)) return;
+        rigidBody.Sleep();
+        transform.SetPositionAndRotation(startPos, startRot);
+    }
+}
