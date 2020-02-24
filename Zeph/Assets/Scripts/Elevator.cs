92e29074c9073a1c8bf2babb9334af89caf735fa
Eoin Ahern <eahern123@gmail.com>
2020-02-24 15:31:38 +0000

    Getting ready for builds

--- Zeph/Assets/Scripts/Elevator.cs
+++ Zeph/Assets/Scripts/Elevator.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.Configuration;
-using UnityEditor.Tilemaps;
-using UnityEngine;
-using UnityEngine.ProBuilder.MeshOperations;
+﻿using UnityEngine;
+
 
 public class Elevator : MonoBehaviour
 {
