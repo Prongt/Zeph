f54d0914af3d5792b1b439589a7a017c359ea2ee
Ryan-Higgins <c16381886@mydit.ie>
2020-03-13 17:54:33 +0000

    Camera fixed in Forest scene

--- Zeph/Assets/ConfineController.cs
+++ Zeph/Assets/ConfineController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ConfineController : MonoBehaviour
 {
     private Transform player;
+    public float zOffset = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,11 @@ public class ConfineController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = new Vector3	(player.transform.position.x, player	);
+        gameObject.transform.position = new Vector3	(player.transform.position.x, player.transform.position.y, player.transform.position.z + zOffset);
+    }
+
+    public void ChangeZOffset()
+    {
+        zOffset = -zOffset;
     }
 }
