2f577fd6d4aff2720e264ceb287656cb36df2c8f
Eoin Ahern <eahern123@gmail.com>
2020-03-15 18:36:50 +0000

    Added pillar fall sound

---
+++ Zeph/Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Cinemachine;
+using FMODUnity;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour
+{
+    [SerializeField] [EventRef] private string collisionEvent;
+    [SerializeField] [TagField] private string[] tagsToIgnore;
+    [SerializeField] private bool playOnce;
+    [SerializeField] private Transform audioPoint;
+    private bool hasPlayed;
+    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        for (int i = 0; i < tagsToIgnore.Length; i++)
+        {
+            if (other.collider.CompareTag(tagsToIgnore[i])) return;
+
+            if (hasPlayed) return;
+           
+            if (playOnce) hasPlayed = true;
+ 
+            RuntimeManager.PlayOneShot(collisionEvent, audioPoint ? audioPoint.position : transform.position);
+        }
+    }
+}
