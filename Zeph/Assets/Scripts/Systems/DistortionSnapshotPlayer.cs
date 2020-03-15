2e40929f5d09f214153a6557d10ce4d5b7ec6879
Eoin Ahern <eahern123@gmail.com>
2020-03-15 21:27:26 +0000

    Distortion stuff

---
+++ Zeph/Assets/Scripts/Systems/DistortionSnapshotPlayer.cs
@@ -0,0 +1,24 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+public class DistortionSnapshotPlayer : MonoBehaviour
+{
+    [SerializeField] private StudioEventEmitter distortionEventEmitter;
+
+    private void Start()
+    {
+        distortionEventEmitter = GetComponent<StudioEventEmitter>();
+    }
+
+    private void Update()
+    {
+        if (Distortion.IsDistorting)
+        {
+            distortionEventEmitter.Play();
+        }
+        else
+        {
+            distortionEventEmitter.Stop();
+        }
+    }
+}
