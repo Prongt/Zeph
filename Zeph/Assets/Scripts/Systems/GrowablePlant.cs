787bed691a026bad7f7845c2354860e222da2537
Eoin Ahern <eahern123@gmail.com>
2020-03-27 16:07:05 +0000

    fixed jumping on flowers without growing them

--- Zeph/Assets/Scripts/Systems/GrowablePlant.cs
+++ Zeph/Assets/Scripts/Systems/GrowablePlant.cs
@@ -2,10 +2,17 @@
 
 public class GrowablePlant : Growable
 {
+    [SerializeField] private bool useTrigger = false;
     protected override void Initialize()
     {
         base.Initialize();
         AspectType = AspectType.GrowablePlant;
+        
+        if (!useTrigger) return;
+        if (colliders.small)
+        {
+            colliders.small.isTrigger = useTrigger;
+        }
     }
 
     public override void Promote(Transform source = null, Element element = null)
@@ -17,6 +24,12 @@ public class GrowablePlant : Growable
         if (colliders.grown)
         {
             colliders.grown.enabled = true;
+            colliders.grown.isTrigger = false;
+        }
+
+        if (colliders.small)
+        {
+            colliders.small.isTrigger = false;
         }
     }
 }
