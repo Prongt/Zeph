ff2b9067a2f7728903b41b25dc1b90dccc70239b
Eoin Ahern <eahern123@gmail.com>
2020-03-28 18:05:46 +0000

    fix

--- Zeph/Assets/Scripts/Movement/PlayerMoveRigidbody.cs
+++ Zeph/Assets/Scripts/Movement/PlayerMoveRigidbody.cs
@@ -128,6 +128,8 @@ namespace Movement
             {
                 zephAnimator = GetComponentInChildren<Animator>();
             }
+
+            zephAnimator.enabled = true;
         }
 
         private IEnumerator MovementDrag()
