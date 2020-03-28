d75022bb479bf6ffa679bb0b58437871a8cce4c2
PKdude96 <C16452312@mydit.ie>
2020-03-28 21:24:33 +0000

    Snow ending update

--- Zeph/Assets/EndingTimelineEventTrigger.cs
+++ Zeph/Assets/EndingTimelineEventTrigger.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class EndingTimelineEventTrigger : MonoBehaviour
 {
+    public PlayableDirector timeline;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider c)
     {
-        
+        if (c.gameObject.tag == "Player")
+        {
+            Debug.Log("LOG LINE IS WORKING");
+            timeline.Play();
+        }
     }
 }
