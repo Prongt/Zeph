7917e4018b0877960d377fb400d9ef1f31554737
Ryan-Higgins <c16381886@mydit.ie>
2020-04-02 18:10:47 +0100

    Map correctly setting button

---
+++ Zeph/Assets/SelectorReset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectorReset : MonoBehaviour
+{
+    public EventSystem eve;
+    
+    public void ResetSelectedButton()
+    {
+        eve.SetSelectedGameObject(null);
+    } 
+}
