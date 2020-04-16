a652fb2e668035733a32042533ed311f3afd859a
Eoin Ahern <eahern123@gmail.com>
2020-04-16 18:13:25 +0100

    pushed script

---
+++ Zeph/Assets/Scripts/TriggerTimeline.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class TriggerTimeline : MonoBehaviour
+{
+    [SerializeField]private PlayableDirector timeline;
+    [SerializeField]private bool triggerOnce = true;
+
+    private bool triggered = false;
+
+
+    public void PlayTimeline()
+    {
+        if (triggerOnce)
+        {
+            if (triggered) return;
+        }
+        timeline.Play();
+        triggered = true;
+    }
+}
